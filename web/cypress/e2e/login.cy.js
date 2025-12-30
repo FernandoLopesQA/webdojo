219835591789867b1a6b278f788555a0a6258698
Fernando de Araujo Lopes <nando.alopes84@gmail.com>
2025-12-30 19:57:47 -0300

    aulas até modulo 3

---
+++ web/cypress/e2e/login.cy.js
@@ -0,0 +1,33 @@
+// Describe para agrupar testes automatizados
+// it para definir um caso de teste
+describe('Login', () => {
+  it('Deve logar com sucesso', () => {
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana123')
+
+    cy.get('[data-cy="user-name"]')
+      .should('be.visible')
+      .and('have.text', 'Fernando Papito')
+
+    cy.get('[data-cy="welcome-message"]')
+      .should('be.visible')
+      .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
+  })
+
+  it('Não deve logar com senha inválida', () => {
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana321')
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+  })
+
+  it('Não deve logar com e-mail não cadastrado', () => {
+    cy.start()
+    cy.submitLoginForm('404@webdojo.com', 'katana123')
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+  })
+
+})
